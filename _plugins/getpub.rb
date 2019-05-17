b04c24e79e7b9c1e8deb54d5d3f7df72f2b128be
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-05-17 12:16:03 +0200

    Minor cleanup with better Ruby

--- _plugins/getpub.rb
+++ _plugins/getpub.rb
@@ -2,6 +2,14 @@ require 'json'
 require 'net/http'
 # require 'yaml'
 
+class String
+  # Requires a name of the form last, first and produces f. last
+  def initials
+    l, f = split ', '
+    "#{f[0,1]}. #{l}"
+  end
+end
+
 module Publications
   class Generator < Jekyll::Generator
     def generate site
@@ -9,10 +17,13 @@ module Publications
       net.use_ssl = true
 
       for _, pub in site.data["publications"]
+        # Make sure the projects are in a list (Not supported yet)
+        # pub["project"] = [pub["project"]] if not pub["project"].kind_of? Array
+
         if pub.key? "inspire-id"
           recid = pub["inspire-id"]
           response = net.get "/api/literature/#{recid}"
-          data = JSON.parse(response.body())["metadata"]
+          data = JSON.parse(response.body)["metadata"]
 
           # Set these *only* if not already set
           pub["title"] ||= data["titles"][0]["title"]
@@ -20,18 +31,14 @@ module Publications
           pub["date"] ||= data["preprint_date"]
 
           # Make the author list, for eventual linking to author pages
-          authors = data["authors"].map{ |a| {
-            "name" => a["full_name"],
-            "id" => a["ids"][0]["value"]
-          }}
+          authors = data["authors"].map do |a|
+            { "name" => a["full_name"], "id" => a["ids"][0]["value"] }
+          end
           pub["authors"] ||= authors
 
           # Build the author string
           # TODO: Does not respect manual author list
-          mini_authors = authors[0..4].map{|a|
-            l, f = a["name"].split ', '
-            "#{f[0,1]}. #{l}"
-          }.join ", "
+          mini_authors = authors[0..4].map{ |a| a["name"].initials }.join ", "
           mini_authors += ' et. al.' if authors.length > authors[0..5].length
 
           # Build the citation string (non-author part)
@@ -46,14 +53,11 @@ module Publications
           pub["citation"] ||= "#{mini_authors}, #{journal}"
           
           if pub.key? 'project'
-            p = site.pages.detect {|page| page.data['shortname'] == pub['project']}
+            p = site.pages.detect { |page| page.data['shortname'] == pub['project']}
             pub['focus-area'] ||= p.data['focus-area'] if p
-            puts "Warning: #{pub['project']} does not exist! Cannot find focus-area." if !p
+            puts "Warning: #{pub['project']} does not exist! Cannot find focus-area." unless p
           end
         end
-
-        # Make sure the projects are in a list (Not supported yet)
-        # pub["project"] = [pub["project"]] if not pub["project"].kind_of? Array
 
         # Uncomment the require yaml and this line to print out each output
         # puts pub.to_yaml
