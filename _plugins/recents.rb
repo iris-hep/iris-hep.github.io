4db8c1ee7a4a53aa12bdcefe6baab80d7b21c421
Henry Schreiner <hschrein@cern.ch>
2021-04-29 12:13:02 -0400

    fix: use git for last modified (#711)

--- _plugins/recents.rb
+++ _plugins/recents.rb
@@ -2,17 +2,15 @@
 
 require 'fileutils'
 require 'pathname'
+require 'jekyll-last-modified-at'
 
 module Recents
   # Generate change information for all markdown pages
   class Generator < Jekyll::Generator
     def generate(site)
-      source = Pathname site.source
       items = site.pages.select { |p| p.path.end_with? '.md' }
-      items.sort_by { |p| (source / p.path).mtime }.each do |page|
-        item = source / page.path
-        dur = item.mtime
-        page.data['last_modified_at_str'] = dur
+      items.each do |page|
+        page.data['last_modified_at_str'] = Jekyll::LastModifiedAt::Determinator.new(site.source, page.path, '%+').to_s
       end
     end
   end
