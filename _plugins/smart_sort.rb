d446af8e4ed7cc7ade2af788132bc2d720e2416c
Henry Schreiner <henryschreineriii@gmail.com>
2021-05-03 21:46:22 -0400

    refactor: move core logic to Ruby

--- _plugins/smart_sort.rb
+++ _plugins/smart_sort.rb
@@ -7,6 +7,14 @@ module IrisHep
       input.select { |p| p['pagetype'] == 'project' && p[field].include?(match) }
            .sort_by { |p| [p['position'] || 0, p['title'].downcase] }
     end
+
+    def get_set(input, key)
+      input.flat_map { |p| p[key] || [] }.uniq
+    end
+
+    def key_to_value(input, hash)
+      input.map { |k| hash.fetch(k, nil) }.compact
+    end
   end
 end
 
