70e9216dca855595f19285e7785c91bb03f36924
Henry Schreiner <henryschreineriii@gmail.com>
2021-05-04 08:53:27 -0400

    refactor: simpler building blocks

--- _plugins/smart_sort.rb
+++ _plugins/smart_sort.rb
@@ -3,15 +3,17 @@
 module IrisHep
   # Adding useful file filters
   module SmartSort
-    def smart_page_sort(input, field, match)
-      input.select { |p| p['pagetype'] == 'project' && p[field].include?(match) }
-           .sort_by { |p| [p['position'] || 0, p['title'].downcase] }
+    # Sort by title, within groups of position if present
+    def smart_title_sort(input)
+      input.sort_by { |p| [p['position'] || 0, p['title'].downcase] }
     end
 
+    # Get a set from a key in a collection of hashes
     def get_set(input, key)
       input.flat_map { |p| p[key] || [] }.uniq
     end
 
+    # Convert array of keys to array of values using a hash
     def key_to_value(input, hash)
       input.map { |k| hash.fetch(k, nil) }.compact
     end
