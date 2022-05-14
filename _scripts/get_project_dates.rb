81ab860a3fc42b290218becbdd7ca41e2d112035
Henry Schreiner <henryschreineriii@gmail.com>
2022-05-14 01:38:57 -0400

    fix: minor rewrite

--- _scripts/get_project_dates.rb
+++ _scripts/get_project_dates.rb
@@ -5,18 +5,37 @@ require 'yaml'
 require 'pathname'
 require 'date'
 
+MAT_TO_KW = {
+  'Development' => 'crit',
+  'Exploratory' => 'crit',
+  'Testing' => 'crit',
+  'Unknown' => 'crit',
+  'Deployed' => 'active',
+  'Archived' => 'done'
+}.freeze
+
+def process_projects(projects)
+  projects.filter_map do |project|
+    project['start-date'] && {
+      title: project['title'],
+      focus_area: [*project['focus-area']],
+      start: project['start-date'],
+      end: project['end-date'] || Date.today,
+      maturity: project['maturity'],
+      status: MAT_TO_KW[project['maturity']]
+    }
+  end
+end
+
 project_files = Pathname.glob('pages/projects/*.md')
 
-projects = project_files.map do |fn|
-  results = YAML.safe_load_file(fn, permitted_classes: [Date])
-  {
-    title: results['title'],
-    focus_area: [*results['focus-area']],
-    start_date: results['start-date'],
-    end_date: results['end-date'] || Date.today,
-    maturity: results['maturity']
-  }
+projects_raw = project_files.map do |fn|
+  YAML.safe_load_file(fn, permitted_classes: [Date])
 end
+
+# missing = projects_raw.filter_map { _1.key?('start-date') ? nil : _1['title'] }
+# puts missing
+projects = process_projects(projects_raw)
 
 ia = projects.filter { _1[:focus_area].include?('ia') }
 as = projects.reject { _1[:focus_area].include?('ia') }
@@ -27,20 +46,9 @@ puts "gantt
     axisFormat  %Y
 "
 
-MAT_TO_KW = {
-  'Development' => 'crit',
-  'Exploratory' => 'crit',
-  'Testing' => 'crit',
-  'Unknown' => 'crit',
-  'Deployed' => 'active',
-  'Archived' => 'done'
-}.freeze
-
 { ia: ia, as: as }.each do |sec, val|
   puts("\n    section #{sec}")
   val.each do |proj|
-    proj[:status] = MAT_TO_KW[proj[:maturity]]
-    msg = "        %<title>-30s  :%<status>-7s, %<start_date>s, %<end_date>s\n"
-    printf(msg, proj) if proj[:start_date]
+    printf("        %<title>-30s  :%<status>-7s, %<start>s, %<end>s\n", proj)
   end
 end
